54af6b17df6dada76c06d68b232aa0d6c03e5e88
srinivaspradhan64 <srinivaspradhan@gmail.com>
2022-10-08 21:18:10 -0400

    Update variables.tf

--- modules/eip/variables.tf
+++ modules/eip/variables.tf
@@ -3,6 +3,6 @@ variable "tags" {
   description = "VPC Tags"
 }
 variable "public_subnet_ids" {
-  type        = map(any)
+  type        = map(map(string))
   description = "VPC Public Subnet mapped to corresponding CIDRs."
 }
