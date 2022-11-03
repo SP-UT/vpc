d085161ab91b6637c839c87773a04282574fbcd8
srinivaspradhan64 <srinivaspradhan@gmail.com>
2022-11-03 19:32:10 -0400

    Update outputs.tf

--- modules/routes/outputs.tf
+++ modules/routes/outputs.tf
@@ -3,18 +3,18 @@ output "public_route_table" {
   description = "Public Route Table ID."
 }
 output "public_route" {
-  value = aws_route.public_routes.id
+  value       = aws_route.public_routes.id
   description = "Public Route ID - sending traffic to Internet Gateway"
 }
 output "route_table_az1" {
-  value = aws_route_table.az1.id
+  value       = aws_route_table.az1.id
   description = "Private Route Table ID - AZ1"
 }
 output "route_table_az2" {
-  value = aws_route_table.az2.id
+  value       = aws_route_table.az2.id
   description = "Private Route Table ID - AZ2"
 }
 output "route_table_az3" {
-  value = aws_route_table.az3.id
+  value       = aws_route_table.az3.id
   description = "Private Route Table ID - AZ3"
 }
